5531c87eb4cdcba4ec7c5bf1b54c247fc1185133
loo <luca.miniati@gmail.com>
2024-12-23 11:10:21 -0800

    Revert "remove ds store"
    This reverts commit 102b9678a38bd1d0d6b8cc9dd5158a1139a7b805.

--- cmake-build-release/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/lucaminiati/math/0x47544f
-# Build directory: /Users/lucaminiati/math/0x47544f/cmake-build-release
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
