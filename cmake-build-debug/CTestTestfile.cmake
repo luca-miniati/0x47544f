102b9678a38bd1d0d6b8cc9dd5158a1139a7b805
loo <luca.miniati@gmail.com>
2024-12-23 11:05:41 -0800

    remove ds store

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/lucaminiati/math/0x47544f
+# Build directory: /Users/lucaminiati/math/0x47544f/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("test")
