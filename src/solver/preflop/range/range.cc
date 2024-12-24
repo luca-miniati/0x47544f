7bbe84ea32452bbcb1e1eeb9cdf96a7cb9a903ab
loo <luca.miniati@gmail.com>
2024-12-24 14:54:52 -0800

    refactor: add ranges

--- src/solver/preflop/range/range.cc
+++ src/solver/preflop/range/range.cc
@@ -3,3 +3,15 @@
 //
 
 #include "range.h"
+
+Range::Range(std::unique_ptr<std::unordered_map<std::string,
+	std::unordered_map<std::shared_ptr<PreflopAction>, double>>> frequencies)
+: frequencies(std::move(frequencies)) {}
+
+double Range::Get(const std::shared_ptr<PreflopAction> &action, const std::string &hand) const {
+	if (!frequencies->contains(hand))
+		throw std::invalid_argument("Hand not recognized: " + hand);
+	if (!frequencies->at(hand).contains(action))
+		return -1.0;
+	return frequencies->at(hand)[action];
+}
