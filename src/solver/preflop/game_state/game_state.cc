0832c0540ed755a6e2510ece0b682b5afebf7fdc
loo <luca.miniati@gmail.com>
2024-12-27 15:00:57 -0800

    idk

---
+++ src/solver/preflop/game_state/game_state.cc
@@ -0,0 +1,5 @@
+//
+// Created by luca miniati on 12/26/24.
+//
+
+#include "game_state.h"
---
+++ src/solver/preflop/game_state/game_state.h
@@ -0,0 +1,16 @@
+//
+// Created by luca miniati on 12/26/24.
+//
+
+#ifndef GAME_STATE_H
+#define GAME_STATE_H
+
+
+
+class GameState {
+
+};
+
+
+
+#endif //GAME_STATE_H
