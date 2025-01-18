df41bc3c4c5783c1c05ad2346c81d78d5d114af6
loo <luca.miniati@gmail.com>
2025-01-18 18:10:14 -0500

    make pot doubles

--- src/solver/game_state/game_state.cc
+++ src/solver/game_state/game_state.cc
@@ -5,16 +5,17 @@
 #include "game_state.h"
 #include <format>
 
-GameState::GameState(const double pot, std::pair<double, double> bets,
+GameState::GameState(std::pair<double, double> pot, std::pair<double, double> bets,
                      const double last_raise, const bool is_terminal, const int num_raises,
                      const int player_to_act, std::string history)
-    : pot(pot), bets(std::move(bets)), last_raise(last_raise), is_terminal(is_terminal),
+    : pot(std::move(pot)), bets(std::move(bets)), last_raise(last_raise), is_terminal(is_terminal),
       num_raises(num_raises), player_to_act(player_to_act), history(std::move(history)) {
 }
 
 std::string GameState::ToString() const {
-    return std::format("{}|({}, {})|{}|{}|{}|P{}|{}",
-        pot,
+    return std::format("({}, {})|({}, {})|{}|{}|{}|P{}|{}",
+        pot.first,
+        pot.second,
         bets.first,
         bets.second,
         last_raise,
--- src/solver/game_state/game_state.h
+++ src/solver/game_state/game_state.h
@@ -7,12 +7,13 @@
 #include <string>
 
 
+
 /**
  * Represents the state of a game of No-Limit Texas Hold'Em.
  */
 struct GameState {
-    std::pair<double, double> bets = {0, 0};
-    double pot = 0, last_raise = 0;
+    std::pair<double, double> pot = {0, 0}, bets = {0, 0};
+    double last_raise = 0;
     bool is_terminal = false;
     int num_raises = 0;
     int player_to_act;
@@ -20,7 +21,8 @@ struct GameState {
 
     /**
      * Constructor for a new GameState.
-     * @param pot a double representing the total size of the pot in big blinds.
+     * @param pot a pair of doubles representing the total contributions of each player to the pot,
+     *            in big blinds.
      * @param bets a pair of doubles representing the current bets of each player, in big blinds.
      * @param last_raise the last raise made by any player (in big blinds), or 0 if there has been
                          no action. A bet is considered a raise from 0 to <bet_size>.
@@ -30,19 +32,21 @@ struct GameState {
      * @param player_to_act the player whose turn it is.
      * @param history a string representation of the actions that have been made.
      */
-    GameState(double pot, std::pair<double, double> bets, double last_raise, bool is_terminal,
-              int num_raises, int player_to_act, std::string history);
+    GameState(std::pair<double, double> pot, std::pair<double, double> bets, double last_raise,
+              bool is_terminal, int num_raises, int player_to_act, std::string history);
 
     /**
      * Returns a string representation of this game state in the following format:
      *
      * <pot>|<bets>|<last_raise>|<is_terminal>|<num_raises>|<player_to_act>|<history>
      *
-     * e.g. "2.0|(2.0, 0)|2|false|1|P2|b(1.0)" would represent the game state after P1 pots it on the
-     *                                         flop in a limped pot.
+     * e.g. "(1, 1)|(2, 0)|2|false|1|P1|b(1.0)" would represent the game state after P1 pots it on the
+     *                                     flop in a limped pot.
      */
     [[nodiscard]] std::string ToString() const;
 };
 
 
+
 #endif //GAME_STATE_H
+
